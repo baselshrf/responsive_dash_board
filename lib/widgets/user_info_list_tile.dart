476ee4125645325934154b5989a420acaabd961d
baselsharaf <waelbk999@gmail>
2026-05-09 11:35:16 +0300

    user info list tile

---
+++ lib/widgets/user_info_list_tile.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+
+class UserInfoListTile extends StatelessWidget {
+  const UserInfoListTile({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subtitle,
+  });
+
+  final String image, title, subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color(0xFFFAFAFA),
+      elevation: 0,
+      child: ListTile(
+        leading: SvgPicture.asset(image),
+        title: Text(
+          title,
+          style: const TextStyle(
+            color: Color(0xFF064060),
+            fontSize: 16,
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.w600,
+            height: 0,
+          ),
+        ),
+        subtitle: Text(subtitle, style: AppStyles.styleRegular12),
+      ),
+    );
+  }
+}
