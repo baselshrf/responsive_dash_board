8799de78692c74a518de049a343a9602a9b75228
baselsharaf <waelbk999@gmail>
2026-05-09 11:15:31 +0300

    generate assets

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const Center(child: Text('Mobile Layout')),
+        tabletLayout: (context) => const Center(child: Text('Tablet Layout')),
+        desktopLayout: (context) => const Center(child: Text('Desktop Layout')),
+      ),
+    );
   }
 }
